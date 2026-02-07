cf1452a3379d3f42c980411035278ab9b8057bef
Travis Martinez <travisesteban91@gmail.com>
2026-02-07 17:40:06 -0500

    I dunno anymore

--- Assets/Scripts/Player/PlayerLoadout.cs
+++ Assets/Scripts/Player/PlayerLoadout.cs
@@ -1,67 +1,130 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using Unity.Netcode;
 
-public class PlayerLoadout : MonoBehaviour
+public class PlayerLoadout : NetworkBehaviour
 {
     [SerializeField] private List<GameObject> weaponPrefabObjList;
-    [SerializeField] private GameObject throwablePrefabObj;
     private List<GameObject> currentWeaponsObjList;
     private int currentWeaponIndex = 0;
-    private GameObject equipedWeapon;
+    private Weapon equippedPrimaryWeapon;
+    private bool isPrimaryFiring = false;
 
-    public void Initialize(PlayerController playerController, Transform weaponMountPoint, Transform throwableMountPoint)
+    [SerializeField] private GameObject throwablePrefabObj;
+    private ThrowableManager throwableManager;
+
+    protected virtual void Update()
     {
+        if (IsServer)
+        {
+            if (equippedPrimaryWeapon != null)
+            {
+                if (isPrimaryFiring) equippedPrimaryWeapon.Fire();
+                if (!isPrimaryFiring) equippedPrimaryWeapon.StopFire();
+            }
+        }
+    }
+
+    public void Initialize(PlayerController playerController)
+    {
+        if (!IsServer) return;
+
         currentWeaponsObjList = new List<GameObject>();
         foreach (GameObject weaponPrefabObj in weaponPrefabObjList)
         {
-            AddWeapon(weaponPrefabObj, playerController, weaponMountPoint);
+            AddWeapon(weaponPrefabObj, playerController);
         }
+        currentWeaponsObjList[0].GetComponent<Weapon>().EquipRpc();
+        equippedPrimaryWeapon = currentWeaponsObjList[0].GetComponent<Weapon>();
 
-        AddThrowable(throwablePrefabObj, throwableMountPoint);
+        AddThrowable(throwablePrefabObj, playerController);
+    }
+
+    public void Deinitialize()
+    {
+        if (!IsServer) return;
+
+        foreach (GameObject weaponObj in currentWeaponsObjList)
+        {
+            weaponObj.GetComponent<Weapon>().Deinitialize();
+            NetworkObject networkObj = weaponObj.GetComponentInParent<NetworkObject>();
+            networkObj.Despawn();
+            Destroy(weaponObj);
+        }
+        currentWeaponsObjList.Clear();
+        currentWeaponIndex = 0;
+        equippedPrimaryWeapon = null;
+
+        if (throwableManager != null)
+        {
+            throwableManager.Deinitialize();
+            NetworkObject networkObj = throwableManager.GetComponentInParent<NetworkObject>();
+            networkObj.Despawn();
+            Destroy(throwableManager.gameObject);
+            throwableManager = null;
+        }
     }
 
 
-    private void AddWeapon(GameObject weaponPrefabObj, PlayerController playerController, Transform weaponMountPoint)
+    private void AddWeapon(GameObject weaponPrefabObj, PlayerController playerController)
     {
-        GameObject newWeapon = GameObject.Instantiate(
+        if (!IsServer) return;
+
+        GameObject newWeapon = Instantiate(
             weaponPrefabObj,
-            weaponMountPoint.position,
-            weaponMountPoint.rotation,
-            weaponMountPoint
+            playerController.weaponMountPoint.position,
+            playerController.weaponMountPoint.rotation
         );
+        NetworkObject networkObj = newWeapon.GetComponent<NetworkObject>();
+        networkObj.Spawn(true);
+        networkObj.TrySetParent(playerController.NetworkObject);
+        networkObj.ChangeOwnership(playerController.OwnerClientId);
+        newWeapon = newWeapon.transform.GetComponentInChildren<Weapon>().gameObject;
         newWeapon.GetComponent<Weapon>().Initialize(playerController);
 
         currentWeaponsObjList.Add(newWeapon);
-
-        if (currentWeaponsObjList.Count - 1 != currentWeaponIndex) newWeapon.GetComponent<Weapon>().Unequip();
-        else newWeapon.GetComponent<Weapon>().Equip();
     }
 
-    private void AddThrowable(GameObject throwablePrefabObj, Transform throwableMountPoint)
+    private void AddThrowable(GameObject throwablePrefabObj, PlayerController playerController)
     {
-        GameObject newThrowable = GameObject.Instantiate(
+        if (!IsServer) return;
+
+        GameObject newThrowable = Instantiate(
             throwablePrefabObj,
-            throwableMountPoint.position,
-            throwableMountPoint.rotation,
-            throwableMountPoint
+            playerController.throwableMountPoint.position,
+            playerController.throwableMountPoint.rotation
         );
-        newThrowable.GetComponent<ThrowableManager>().Initialize(transform);
+        NetworkObject networkObj = newThrowable.GetComponent<NetworkObject>();
+        networkObj.Spawn(true);
+        networkObj.TrySetParent(playerController.NetworkObject);
+        networkObj.ChangeOwnership(playerController.OwnerClientId);
+        throwableManager = newThrowable.GetComponentInChildren<ThrowableManager>();
+        throwableManager.Initialize(playerController);
     }
 
-
-    public void NextWeapon(InputAction.CallbackContext _)
+    [Rpc(SendTo.Server)]
+    public void NextWeaponRpc()
     {
-        currentWeaponsObjList[currentWeaponIndex].GetComponent<Weapon>().Unequip();
+        currentWeaponsObjList[currentWeaponIndex].GetComponent<Weapon>().UnequipRpc();
         currentWeaponIndex = (currentWeaponIndex + 1) % currentWeaponsObjList.Count;
-        currentWeaponsObjList[currentWeaponIndex].GetComponent<Weapon>().Equip();
+        currentWeaponsObjList[currentWeaponIndex].GetComponent<Weapon>().EquipRpc();
+        equippedPrimaryWeapon = currentWeaponsObjList[currentWeaponIndex].GetComponent<Weapon>();
+    }
+    [Rpc(SendTo.Server)]
+    public void PreviousWeaponRpc()
+    {
+        currentWeaponsObjList[currentWeaponIndex].GetComponent<Weapon>().UnequipRpc();
+        currentWeaponIndex = (currentWeaponIndex - 1 + currentWeaponsObjList.Count) % currentWeaponsObjList.Count;
+        currentWeaponsObjList[currentWeaponIndex].GetComponent<Weapon>().EquipRpc();
+        equippedPrimaryWeapon = currentWeaponsObjList[currentWeaponIndex].GetComponent<Weapon>();
     }
 
-    public void PreviousWeapon(InputAction.CallbackContext _)
-    {
-        currentWeaponsObjList[currentWeaponIndex].GetComponent<Weapon>().Unequip();
-        currentWeaponIndex = (currentWeaponIndex - 1 + currentWeaponsObjList.Count) % currentWeaponsObjList.Count;
-        currentWeaponsObjList[currentWeaponIndex].GetComponent<Weapon>().Equip();
-    }
+    [Rpc(SendTo.Server)]
+    public void OnPrimaryFireStartedRpc() => isPrimaryFiring = true;
+    [Rpc(SendTo.Server)]
+    public void OnPrimaryFireCanceledRpc() => isPrimaryFiring = false;
+    [Rpc(SendTo.Server)]
+    public void OnThrowableStartedRpc() => throwableManager.StartThrow();
+    [Rpc(SendTo.Server)]
+    public void OnThrowableCanceledRpc() => throwableManager.ReleaseThrow();
 }
