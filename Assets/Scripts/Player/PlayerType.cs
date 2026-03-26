a73c1a930e3b4983d2a2d50bd3323135d1825702
BarelyFunctionalCode <nicholasandrewchadwick@gmail.com>
2026-03-26 11:31:18 -0500

    New player legs and arm rotation, added footstep particles.

--- Assets/Scripts/Player/PlayerType.cs
+++ Assets/Scripts/Player/PlayerType.cs
@@ -11,6 +11,13 @@ public class PlayerType : NetworkBehaviour
     [SerializeField] public Transform throwableMountPoint;
     [SerializeField] public AudioSource hoverAudioSource;
     [SerializeField] public AudioSource windAudioSource;
+    [SerializeField] private ParticleSystem animationFootstepParticleSystem;
+
+    [SerializeField] private Transform legsDirectionTransform;
+
+    private Quaternion desiredLegsDirection;
+    private Quaternion currentLegsDirection;
+    [SerializeField] private float legsRotateSpeed = 10f;
 
     [SerializeField] public float mass = 1f;
 
@@ -22,6 +29,41 @@ public class PlayerType : NetworkBehaviour
         {
             playerController.OnPlayerTypeObjectSpawned(this);
         }
+    }
+
+    public void HandleLegRotation(Vector3 movementDirection)
+    {
+        // Get current movement direction from player controller
+        if (movementDirection != Vector3.zero) 
+            desiredLegsDirection = Quaternion.LookRotation(
+                Vector3.ProjectOnPlane(movementDirection, Vector3.up).normalized,
+                Vector3.up
+            ).normalized;
+
+        // Determine how closely aligned the current legs direction is with the desired direction
+        float angleAlignment = Mathf.Abs(Quaternion.Dot(currentLegsDirection, desiredLegsDirection));
+
+        // Snap-to or smoothly rotate legs direction based on alignment. The closer they are aligned, the faster they rotate.
+        if (angleAlignment > 0.999f) currentLegsDirection = desiredLegsDirection;
+        else currentLegsDirection = Quaternion.Slerp(
+            currentLegsDirection,
+            desiredLegsDirection,
+            Time.deltaTime * legsRotateSpeed * (angleAlignment + 0.1f)
+        );
+
+        // Set transform that constrains the pelvis of the model armature.
+        legsDirectionTransform.rotation = Quaternion.LookRotation(currentLegsDirection * Vector3.forward, Vector3.up);
+    }
+
+    public void AnimationFootstepEvent(int footIndex)
+    {
+        var emitParams = new ParticleSystem.EmitParams();
+        Vector3 footPosition = animationFootstepParticleSystem.transform.position;
+        footPosition += transform.right * (footIndex == 0 ? -0.6f : 0.6f);
+        emitParams.rotation = legsDirectionTransform.eulerAngles.y;
+        emitParams.position = footPosition;
+        emitParams.applyShapeToPosition = true;
+        animationFootstepParticleSystem.Emit(emitParams, 1);
     }
 
     public void OnDie()
